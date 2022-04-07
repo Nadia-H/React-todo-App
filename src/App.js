00a243441f1152a1fa7b86e308793901233009a4
Nadia-H <nhylary12@gmail.com>
2022-04-07 19:26:49 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //states
+    const [inputText, setInputText] = useState('');
+    const [todos, setTodos] = useState([])
+    const [status, setStatus] = useState("all")
+    const [filteredTodos, setFilteredTodos] = useState([])
+
+    //RUN ONCE
+    useEffect(()=>{
+        getLocalTodos()
+    },[])
+    //USE EFFECT 
+    useEffect(()=>{
+        filterHandler()
+        saveLocalTodos()
+    }, [todos, status])
+    //functions
+    const filterHandler = ()=>{
+        switch(status){
+            case 'completed':
+                setFilteredTodos(todos.filter(todo=> todo.completed === true))
+                break
+            case 'uncompleted':
+                setFilteredTodos(todos.filter(todo=> todo.completed === false))
+                break
+            default:
+                setFilteredTodos(todos)
+                break
+        }
+    }
+    const saveLocalTodos = ()=>{ 
+        localStorage.setItem('todos', JSON.stringify(todos))
+        
+    }
+    const getLocalTodos = ()=>{
+        if(localStorage.getItem('todos')===null){
+            localStorage.setItem('todos', JSON.stringify([]))
+        }
+        else{
+            let localtodos = JSON.parse(localStorage.getItem('todos')) //, JSON.stringify(todos))
+            setTodos(localtodos)
+        }
+    }
+    return (
+        <div className="App">
+        <header>
+            <h1>NH's Todo React App</h1>
+        </header>
+        <Form 
+            todos = {todos}
+            setTodos={setTodos} 
+            inputText={inputText} 
+            setInputText={setInputText}
+            setStatus={setStatus}
+            />
+        <TodoList  
+            setTodos={setTodos} 
+            todos={todos}
+            filteredTodos={filteredTodos}/>
+        </div>
   );
 }
 
